490439897bfd493d0e9534478d80d7ece095be97
姚丰利 <memory_my_love@163.com>
2015-12-31 17:12:37 +0800

    1.合并分支,调试bug<br/>

--- app/scheduler/method.py
+++ app/scheduler/method.py
@@ -3,12 +3,13 @@ from datetime import datetime
 from app.core.tools import get_uuid
 from app.model.job_progress_model import JobProgress
 from app.model.script_model import Script
-from app.script.thread import Thread
+from app.script.job_thread import JobThread
 
 
 class runMethod:
     # def __init__(self):
     #     print ("init f u ")
+
     def doing(self):
         print("f u very much")
 
@@ -16,8 +17,8 @@ class runMethod:
         print('Tick! The time is: %s' % datetime.now())
 
     def runJob(self, jobId):
-        #查询job下所有的脚本
-        #job_id = self.args.get("job_id")
+        # 查询job下所有的脚本
+        # job_id = self.args.get("job_id")
         list = Script.select().where(Script.job_id == jobId)
         max_num = len(list);
         pro = JobProgress()
@@ -30,9 +31,9 @@ class runMethod:
         pro.result = -1;
         pro.save(force_insert=True)
 
-        #开启线程执行脚本 并保存执行记录
-        #for i in list:
-        Thread(pro.uuid);
+        # 开启线程执行脚本 并保存执行记录
+        # for i in list:
+        JobThread(pro.uuid);
 
-    def tick(self,name):
-        print('Tick! The time is: %s' % datetime.now()+name)
+    def tick(self, name):
+        print('Tick! The time is: %s' % datetime.now() + name)
