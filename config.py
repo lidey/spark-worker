bf7fd66c56c8a0c66dc0e1330980d0389f93d7ef
姚丰利 <memory_my_love@163.com>
2016-01-11 14:31:14 +0800

    1.修改配置信息

--- config.py
+++ config.py
@@ -5,31 +5,41 @@
 # File Author: lidey 
 # File Created Date: 2015-11-27 17:31
 
-# 数据库配置信息
 import logging
 
+# 服务配置信息
+system = {
+    'name': '大数据云--数据调度系统',
+    'hostname': '127.0.0.1',
+    'port': 8880,
+    'company': '软通动力信息技术（集团）有限公司',
+    'version': '0.0.1'
+}
+
+# 数据库配置
 database = {
-    # 'host': '10.211.55.15',
-    'host': '127.0.0.1',
+    'host': '10.211.55.15',
+    # 'host': '127.0.0.1',
     'port': 3306,
-    # 'database': 'cloud-data',
-    # 'user': 'cloud-data',
-    # 'password': 'cloud-data',
-    'database': 'yxk',
-    'user': 'root',
-    'password': 'root',
+    'database': 'cloud-data',
+    'user': 'cloud-data',
+    'password': 'cloud-data',
+    # 'database': 'yxk',
+    # 'user': 'root',
+    # 'password': 'root',
     'max_connections': 10,
     'stale_timeout': 300,
     'charset': 'utf8',
 }
+
+# 缓存配置
 redis = {
     'host': '10.211.55.15',
     'port': 6379,
     'db': 11,
 }
 
+# 日志配置
 logger = {
     'level': logging.DEBUG
 }
-# web服务配置信息
-port = 8880
